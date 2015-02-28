7f4e44360d87943df1f2397ce1de07aebd9d2db7
Bide Yan <b6yan@uwaterloo.ca>
2015-02-28 02:58:52 -0500

    fix activity not found bug

--- src/com/fami/chat/FamiChatActivity.java
+++ src/com/fami/chat/FamiChatActivity.java
@@ -14,6 +14,7 @@ import java.util.List;
 
 import com.fami.MainActivity;
 import com.fami.R;
+import com.fami.user.LogInActivity;
 
 public class FamiChatActivity extends ActionBarActivity {
 
@@ -71,9 +72,8 @@ public class FamiChatActivity extends ActionBarActivity {
 
     @Override
     public void onBackPressed() {
-        Intent i = new Intent(FamiChatActivity.this, MainActivity.class);
+        Intent i = new Intent(FamiChatActivity.this, LogInActivity.class);
         startActivity(i);
         finish();
-
     }
 }
