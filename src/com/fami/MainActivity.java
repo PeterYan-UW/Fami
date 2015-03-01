58a8c98f6578e86e70f7f3fd38894b41a898eca2
DING LI <d76li@uwaterloo.ca>
2015-03-01 03:00:20 -0500

    add todolist but it doesnot work

--- src/com/fami/MainActivity.java
+++ src/com/fami/MainActivity.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import com.fami.R;
 import com.fami.chat.ChatActivity;
+import com.fami.todolist.TodolistActivity;
 import com.fami.user.UpdateActivity;
 import com.fami.user.helper.DataHolder;
 import com.quickblox.chat.QBChatService;
@@ -55,7 +56,19 @@ public class MainActivity extends BaseActivity{
                 break;
         }
     }
-
+	
+	public static void Ontodolist(View view) {
+		switch (view.getId()) {
+        case R.id.todolist:
+            progressDialog.show();
+            Intent intent = new Intent(this, TodolistActivity.class);
+            startActivity(intent);
+            finish();
+            break;
+        case R.id.sign_up:
+        	break;
+		}
+	}
 	public static void start(Context context, Bundle bundle) {
         Intent intent = new Intent(context, MainActivity.class);
         intent.putExtras(bundle);
