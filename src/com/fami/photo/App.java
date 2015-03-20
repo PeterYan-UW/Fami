f8e87d391a10555c99387aaf70f016cfc64f7259
Zhehan Xue <z9xue@uwaterloo.ca>
2015-03-20 13:55:16 -0400

    add photo function

--- src/com/fami/photo/App.java
+++ src/com/fami/photo/App.java
@@ -1,31 +1,23 @@
-package com.fami.user.helper;
+package com.fami.photo;
 
+import android.annotation.TargetApi;
 import android.app.Application;
 import android.content.Context;
 import android.os.Build;
 import android.os.StrictMode;
-
-import com.fami.photo.utils.Constants;
 import com.nostra13.universalimageloader.cache.disc.naming.Md5FileNameGenerator;
 import com.nostra13.universalimageloader.core.ImageLoader;
 import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
 import com.nostra13.universalimageloader.core.assist.QueueProcessingType;
-import com.quickblox.chat.model.QBDialog;
-import com.quickblox.users.model.QBUser;
+import com.fami.photo.utils.Constants;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+public class App extends Application {
 
-public class ApplicationSingleton extends Application {
-
-    private QBUser currentUser;
-
-    private Map<Integer, QBUser> dialogsUsers = new HashMap<Integer, QBUser>();
-
+    @TargetApi(Build.VERSION_CODES.GINGERBREAD)
+    @SuppressWarnings("unused")
     @Override
     public void onCreate() {
-    	if (Constants.CONFIG_DEVELOPER_MODE && Build.VERSION.SDK_INT >= Build.VERSION_CODES.GINGERBREAD) {
+        if (Constants.CONFIG_DEVELOPER_MODE && Build.VERSION.SDK_INT >= Build.VERSION_CODES.GINGERBREAD) {
             StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder().detectAll().penaltyDialog().build());
             StrictMode.setVmPolicy(new StrictMode.VmPolicy.Builder().detectAll().penaltyDeath().build());
         }
@@ -35,44 +27,6 @@ public class ApplicationSingleton extends Application {
         initImageLoader(getApplicationContext());
     }
 
-
-    public QBUser getCurrentUser() {
-        return currentUser;
-    }
-
-    public void setCurrentUser(QBUser currentUser, String userPassword) {
-        this.currentUser = currentUser;
-        this.currentUser.setPassword(userPassword);
-    }
-
-    public Map<Integer, QBUser> getDialogsUsers() {
-        return dialogsUsers;
-    }
-
-    public void setDialogsUsers(List<QBUser> setUsers) {
-        dialogsUsers.clear();
-
-        for (QBUser user : setUsers) {
-            dialogsUsers.put(user.getId(), user);
-        }
-    }
-
-    public void addDialogsUsers(List<QBUser> newUsers) {
-        for (QBUser user : newUsers) {
-            dialogsUsers.put(user.getId(), user);
-        }
-    }
-
-    public Integer getOpponentIDForPrivateDialog(QBDialog dialog){
-        Integer opponentID = -1;
-        for(Integer userID : dialog.getOccupants()){
-            if(!userID.equals(getCurrentUser().getId())){
-                opponentID = userID;
-                break;
-            }
-        }
-        return opponentID;
-    }
     public static void initImageLoader(Context context) {
         // This configuration tuning is custom. You can tune every option, you may tune some of them,
         // or you can create default configuration by
---
+++ src/com/fami/photo/utils/OnGetImageFileListener.java
@@ -0,0 +1,8 @@
+package com.fami.photo.utils;
+
+import java.io.File;
+
+public interface OnGetImageFileListener {
+
+    public void onGotImageFile(File imageFile);
+}
