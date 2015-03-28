69fb268031e48599eef8705966626e9c6ef2c00e
Jazbel Wang <c278wang@uwaterloo.ca>
2015-03-28 18:09:02 -0400

    with map

---
+++ src/com/fami/whereabouts/utils/Constants.java
@@ -0,0 +1,15 @@
+package com.fami.whereabouts.utils;
+
+public class Constants {
+
+    public static final int APP_ID = 99;
+    public static final String AUTH_KEY = "63ebrp5VZt7qTOv";
+    public static final String AUTH_SECRET = "YavMAxm5T59-BRw";
+
+    public static final  String USER_LOGIN = "test1@b.com";
+    public static final  String USER_PASSWORD = "123456789";
+    
+    public static int LOCATION_MIN_TIME = 20000;
+    public static int LOCATION_PER_PAGE = 100;
+    public static int PLAY_SERVICE_REQUEST_CODE = 10;
+}
