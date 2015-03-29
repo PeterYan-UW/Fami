95ff285035066ba470fe12d8dcf3988361e088d6
Bide Yan <b6yan@uwaterloo.ca>
2015-03-29 17:12:40 -0400

    temp add

--- src/com/fami/family/CreateFami.java
+++ src/com/fami/family/CreateFami.java
@@ -1,12 +1,26 @@
 package com.fami.family;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 
 import com.fami.BaseActivity;
 import com.fami.R;
 import com.fami.user.activities.LogInActivity;
+import com.fami.user.helper.DataHolder;
+import com.quickblox.chat.QBChatService;
+import com.quickblox.core.QBEntityCallbackImpl;
+import com.quickblox.core.request.QBRequestGetBuilder;
+import com.quickblox.customobjects.QBCustomObjects;
+import com.quickblox.customobjects.model.QBCustomObject;
 
 public class CreateFami extends BaseActivity{
 	
@@ -19,18 +33,79 @@ public class CreateFami extends BaseActivity{
 	public void onClick(View view) {
         switch (view.getId()) {
 	        case R.id.create_fami:
-	        	Intent create_fami = new Intent(this, FamiActivity.class);
+	        	Intent create_fami = new Intent(this, CreateFamiActivity.class);
 	            startActivity(create_fami);
 	            finish();
 	            break;
 	        case R.id.join_fami:
-	        	Intent join_fami = new Intent(this, JoinFamiActivity.class);
-	            startActivity(join_fami);
-	            finish();
+        		AlertDialog.Builder searchBuilder = new AlertDialog.Builder(this);
+        		searchBuilder.setTitle("Search for Fami");
+        		searchBuilder.setMessage("Please type in your Fami ID?");
+        		final EditText inputField = new EditText(this);
+        		searchBuilder.setView(inputField);
+        		searchBuilder.setPositiveButton("Search", new DialogInterface.OnClickListener() {
+        			@Override
+        			public void onClick(DialogInterface dialogInterface, int i) {
+        				final String FamiID = inputField.getText().toString();
+        				QBRequestGetBuilder famiRequestBuilder = new QBRequestGetBuilder();
+        				famiRequestBuilder.eq("_id", FamiID);
+        				QBCustomObjects.getObjects("Fami", famiRequestBuilder ,new QBEntityCallbackImpl<ArrayList<QBCustomObject>>() {
+        					@Override
+        				    public void onSuccess(final ArrayList<QBCustomObject> famiObjects, Bundle params) {
+        						Log.v("search success", "search success " + famiObjects.size());
+        				    	if (famiObjects.size() == 1){
+            		                AlertDialog.Builder fami_join = new AlertDialog.Builder(CreateFami.this);
+            		                fami_join.setMessage("Found a Fami with FamiID: " + FamiID);
+            		                fami_join.setPositiveButton("Join", new DialogInterface.OnClickListener() {
+
+										@Override
+										public void onClick(DialogInterface arg0, int arg1) {
+											JoinFami(famiObjects.get(0));									
+										}
+
+            		                	
+            		                });
+            		                fami_join.show(); 
+        				    	}
+        				    }
+
+        					@Override
+        				    public void onError(List<String> errors) {
+        		                AlertDialog.Builder fami_error = new AlertDialog.Builder(CreateFami.this);
+        		                fami_error.setMessage("get Fami errors: " + errors).create().show();
+        		                fami_error.show();
+        				    }
+        				});
+        			}
+        		});
+
+        		searchBuilder.setNegativeButton("Cancel",null);
+
+        		searchBuilder.create().show();
 	            break;
 	        	
         }
     }
+	private void JoinFami(QBCustomObject qbCustomObject) {
+		HashMap<String, Object> fields = qbCustomObject.getFields();
+		ArrayList<Integer> member_id = (ArrayList<Integer>) fields.get("member_id");
+		member_id.add(DataHolder.getDataHolder().getSignInUserId());
+		fields.remove("member_id");
+		fields.put("member_id", member_id);
+		qbCustomObject.setFields(fields);
+//		QBCustomObjects.updateObject(qbCustomObject, new QBEntityCallbackImpl<QBCustomObject>() {
+//    	    @Override
+//    	    public void onSuccess(QBCustomObject createdObject, Bundle bundle) {
+//    	    	
+//    	    }
+//    	 
+//    	    @Override
+//    	    public void onError(List<String> errors) {
+//    	 
+//    	    }
+//    	});
+		
+	}
 	public void onBackPressed(){
 		Intent logout = new Intent(this, LogInActivity.class);
 		startActivity(logout);
--- src/com/fami/family/JoinFamiActivity.java
+++ src/com/fami/family/JoinFamiActivity.java
@@ -1,81 +1,99 @@
-package com.fami.family;
-
-import java.util.ArrayList;
-import java.util.List;
-
-import android.content.Intent;
-import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.view.ViewPager;
-import android.support.v7.app.ActionBar;
-import android.support.v7.app.ActionBarActivity;
-
-import com.fami.BaseActivity;
-import com.fami.R;
-import com.fami.family.FamiActivity.SectionsPagerAdapter;
-import com.fami.user.UsersFragment;
-import com.fami.user.activities.LogInActivity;
-
-public class JoinFamiActivity extends ActionBarActivity {
-
-    private SectionsPagerAdapter sectionsPagerAdapter;
-    private ViewPager viewPager;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        final ActionBar actionBar = getSupportActionBar();
-        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
-
-        List<Fragment> tabs = new ArrayList<Fragment>();
-        tabs.add(UsersFragment.getInstance());
-
-        sectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager(), tabs);
-
-        viewPager = (ViewPager) findViewById(R.id.pager);
-        viewPager.setAdapter(sectionsPagerAdapter);
-
-        viewPager.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
-            @Override
-            public void onPageSelected(int position) {
-                actionBar.setSelectedNavigationItem(position);
-            }
-        });
-    }
-
-    public static class SectionsPagerAdapter extends FragmentPagerAdapter {
-
-        private List<Fragment> fragments;
-
-        public SectionsPagerAdapter(FragmentManager fm, List<Fragment> fragments) {
-            super(fm);
-            this.fragments = fragments;
-        }
-
-        @Override
-        public Fragment getItem(int position) {
-            return fragments.get(position);
-        }
-
-        @Override
-        public int getCount() {
-            return fragments.size();
-        }
-
-        @Override
-        public CharSequence getPageTitle(int position) {
-            return "Users";
-        }
-    }
-
-    @Override
-    public void onBackPressed() {
-        Intent i = new Intent(JoinFamiActivity.this, LogInActivity.class);
-        startActivity(i);
-        finish();
-    }
-}
+//package com.fami.family;
+//
+//import java.util.ArrayList;
+//import java.util.Collection;
+//import java.util.List;
+//
+//import android.app.Activity;
+//import android.app.AlertDialog;
+//import android.content.Intent;
+//import android.os.Bundle;
+//import android.support.v4.app.Fragment;
+//import android.support.v4.app.FragmentManager;
+//import android.support.v4.app.FragmentPagerAdapter;
+//import android.support.v4.view.ViewPager;
+//import android.support.v7.app.ActionBar;
+//import android.support.v7.app.ActionBarActivity;
+//import android.view.MenuInflater;
+//import android.view.MenuItem;
+//import android.view.View;
+//import android.widget.AdapterView;
+//import android.widget.ListView;
+//import android.widget.ProgressBar;
+//
+//import com.fami.BaseActivity;
+//import com.fami.R;
+//import com.fami.chat.ChatActivity;
+//import com.fami.family.CreateFamiActivity.SectionsPagerAdapter;
+//import com.fami.user.UsersFragment;
+//import com.fami.user.activities.LogInActivity;
+//import com.fami.user.helper.ApplicationSingleton;
+//import com.quickblox.chat.QBChatService;
+//import com.quickblox.chat.model.QBDialog;
+//import com.quickblox.chat.model.QBDialogType;
+//import com.quickblox.core.QBEntityCallbackImpl;
+//import com.quickblox.core.request.QBPagedRequestBuilder;
+//import com.quickblox.core.request.QBRequestGetBuilder;
+//import com.quickblox.customobjects.QBCustomObjects;
+//import com.quickblox.customobjects.model.QBCustomObject;
+//import com.quickblox.users.QBUsers;
+//import com.quickblox.users.model.QBUser;
+//
+//public class JoinFamiActivity  extends Activity {
+//
+//    private ListView famiListView;
+//    private ProgressBar progressBar;
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.join_family_activity);
+//
+//        famiListView = (ListView) findViewById(R.id.familiesList);
+//        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+//
+//
+//        // get dialogs
+//        //
+//        QBRequestGetBuilder customObjectRequestBuilder = new QBRequestGetBuilder();
+//        customObjectRequestBuilder.setPagesLimit(100);
+//        QBCustomObjects.getObjects("Fami", new QBEntityCallbackImpl<ArrayList<QBCustomObject>>() {
+//            @Override
+//            public void onSuccess(final ArrayList<QBCustomObject> famis, Bundle args) {
+//            	buildListView(famis);
+//            }
+//
+//            @Override
+//            public void onError(List<String> errors) {
+//                AlertDialog.Builder dialog = new AlertDialog.Builder(JoinFamiActivity.this);
+//                dialog.setMessage("get Fami errors: " + errors).create().show();
+//            }
+//        });
+//    }
+//
+//
+//    void buildListView(List<QBCustomObject> Famis){
+//        final FamilyAdapter adapter = new FamilyAdapter(Famis, JoinFamiActivity.this);
+//        famiListView.setAdapter(adapter);
+//
+//        progressBar.setVisibility(View.GONE);
+//
+//        // choose dialog
+//        //
+//        dialogsListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//            @Override
+//            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+//                QBDialog selectedDialog = (QBDialog)adapter.getItem(position);
+//
+//                Bundle bundle = new Bundle();
+//                bundle.putSerializable(ChatActivity.EXTRA_DIALOG, (QBDialog)adapter.getItem(position));
+//
+//                // group
+//                bundle.putSerializable("mode", "Group");
+//                // Open chat activity
+//                //
+//                ChatActivity.start(JoinFamiActivity.this, bundle);
+//            }
+//        });
+//    }
+//}
